490c524e9689c00b4b0270e5a785475f64114b5c
Satyakrishna Jasti <satyajasti9@gmail.com>
2019-11-02 22:41:37 +0530

    Create Bignum.py

---
+++ Bignum.py
@@ -0,0 +1,10 @@
+n1=input("Enter the number n1: ");
+n2=input("Enter the number n2: ");
+n3=input("Enter the numebr n3: ");
+
+if (n1>n2) and (n1>n3):
+    print "N1 Big"
+elif (n2>n1) and (n2>n3):
+    print "N2 Big";
+else:
+    print "N3 Big";

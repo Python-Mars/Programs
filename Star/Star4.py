0f997ada1aac7c2d6b41233b459bd40615afe7aa
Satyakrishna Jasti <satyajasti9@gmail.com>
2019-11-14 11:51:42 +0530

    Create Star4.py

---
+++ Star/Star4.py
@@ -0,0 +1,24 @@
+row=int(input("enter the rows"))
+
+for i in range (row,0,-1):
+    for j in range(0,i):
+        print("*",end=' ')
+    print ("\n")
+    
+    
+    
+    
+    
+    
+O/P:
+
+enter the rows5
+* * * * * 
+
+* * * * 
+
+* * * 
+
+* * 
+
+* 

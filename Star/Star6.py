f0ec532608dfec2ac4eb3a2e8d8ec7bc8ca08a46
Satyakrishna Jasti <satyajasti9@gmail.com>
2019-11-19 13:58:24 +0530

    Create Star6.py

---
+++ Star/Star6.py
@@ -0,0 +1,23 @@
+for i in range(0,6):
+    for j in range(0,6):
+        if((i-j)==0)or(i+j==5):
+            print("*",end=' ')
+        print(" ",end=' ')
+    print("\n")
+    
+    
+    
+    
+    O/P:
+    
+*           *   
+
+  *       *     
+
+    *   *       
+
+    *   *       
+
+  *       *     
+
+*           *   

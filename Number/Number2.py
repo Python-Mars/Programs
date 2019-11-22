e5203f08ea110771b796d9f3ae53177fc7b12952
Satyakrishna Jasti <satyajasti9@gmail.com>
2019-11-22 12:49:45 +0530

    Create Number2.py

---
+++ Number/Number2.py
@@ -0,0 +1,14 @@
+for i in range(0,6):
+    for j in range(0,6):
+        print(j,end=' ')
+    print()
+    
+    
+    O/P:
+
+0 1 2 3 4 5 
+0 1 2 3 4 5 
+0 1 2 3 4 5 
+0 1 2 3 4 5 
+0 1 2 3 4 5 
+0 1 2 3 4 5 

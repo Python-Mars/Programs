b1ec9e3babfd2fc380cd01ce2be06688532371de
Jasti9 <satyajasi9@gmail.com>
2019-11-22 13:04:53 +0530

    add

---
+++ Table/Table.py
@@ -0,0 +1,26 @@
+n=int(input("Enter the number: "))
+print("\n")
+print(n,"th Table")
+print("\n")
+for i in range(1,11):
+    print(n,"*",i,"=",n*i)
+
+
+O/P:
+
+Enter the number: 100
+
+
+100 th Table
+
+
+100 * 1 = 100
+100 * 2 = 200
+100 * 3 = 300
+100 * 4 = 400
+100 * 5 = 500
+100 * 6 = 600
+100 * 7 = 700
+100 * 8 = 800
+100 * 9 = 900
+100 * 10 = 1000

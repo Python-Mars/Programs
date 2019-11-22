dd628826d5227e1a815343958b8ace044178ceb9
Satyakrishna Jasti <satyajasti9@gmail.com>
2019-11-22 12:50:56 +0530

    Create Number3.py

---
+++ Number/Number3.py
@@ -0,0 +1,16 @@
+for i in range(6,0,-1):
+    for j in range(0,i):
+        print(j,end=' ')
+    print()
+    
+    
+    
+    
+    O/P:
+        
+0 1 2 3 4 5 
+0 1 2 3 4 
+0 1 2 3 
+0 1 2 
+0 1 
+0 

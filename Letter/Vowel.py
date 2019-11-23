b2cc07f0b4489dd0fe2531c2d911098f4b0ff3b5
Satyakrishna Jasti <satyajasti9@gmail.com>
2019-11-23 10:05:38 +0530

    Create Vowel.py

---
+++ Letter/Vowel.py
@@ -0,0 +1,19 @@
+print("Vowels\n")
+for x in range(65,91):
+    if(x==65)or (x==69) or(x==73) or(x==79) or(x==85) :
+        print(chr(x),end=' ')
+        print("\n")
+        
+  O/P:
+  
+Vowels
+
+A 
+
+E 
+
+I 
+
+O 
+
+U 

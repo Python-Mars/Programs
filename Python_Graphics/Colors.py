f5eebf2d8d40f17f1ebac8e3bd87703525587df4
Satyakrishna Jasti <satyajasti9@gmail.com>
2019-11-23 15:00:06 +0530

    Update Colors.py

--- Python_Graphics/Colors.py
+++ Python_Graphics/Colors.py
@@ -5,3 +5,6 @@ for x in range(150):
     width(x/10 + 1)
     forward(x)
     left(59)
+
+    
+    ![Screen Shot 2019-11-23 at 2 52 49 PM](https://user-images.githubusercontent.com/34743233/69476595-d89ac800-0e01-11ea-9667-becbf1703b22.png)
